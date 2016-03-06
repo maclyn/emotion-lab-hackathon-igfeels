47a35c1e93848354194868fd6b468e0ed9ff93ff
Maclyn Brandwein <maclyn@users.noreply.github.com>
2016-03-06 11:00:39 -0500

    This... was not clever.
    The old ID/secret have been revoked already.

--- ImageDetectorDemo/src/main/java/com/hackathon/igfeels/instagramApi/ApplicationData.java
+++ ImageDetectorDemo/src/main/java/com/hackathon/igfeels/instagramApi/ApplicationData.java
@@ -1,7 +1,7 @@
 package com.hackathon.igfeels.instagramApi;
 
 public class ApplicationData {
-    public static final String CLIENT_ID = "358b2d53b0384d5e9c521125baa11489";
-    public static final String CLIENT_SECRET = "1584e301c2ee49099a57799fa324544f";
+    public static final String CLIENT_ID = "id";
+    public static final String CLIENT_SECRET = "secret";
     public static final String CALLBACK_URL = "instagram://connect";
 }
